35121c1beade273368677a10ff6d98edc6c9f160
brn_2_b_wild@hotmail.com <brn2bwild>
2025-10-31 10:51:27 -0600

    Modificada la ecuación del PID. Calibrados algunos valores de KP y KD.

--- BarraSensores16.cpp
+++ BarraSensores16.cpp
@@ -91,9 +91,9 @@ int BarraSensores16::proporcional() {
   if (y > 0) {
     p = x / y;
   } else if (y == 0 && x1 > 750) {
-    p = 1500;
+    p = 1450;
   } else if (y == 0 && x1 < 750) {
-    p = 0;
+    p = 50;
   }
 
   return p;
